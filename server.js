8940f03eb9edf8bf402d4f8bbe1ac12098472f2e
AinoMegumi <aimegu2014@gmail.com>
2022-09-02 01:32:16 +0900

    クォーテーションを変更

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const multer = require('multer');
 const sortFile = require('./sortfile.js');
-const upload = multer({ dest: path.dirname(__dirname).replace(/\\/g, "/") + '/files/' });
+const upload = multer({ dest: path.dirname(__dirname).replace(/\\/g, '/') + '/files/' });
 
 const app = express();
 app.post('/api/upload', upload.array('files'), (req, res) => {
