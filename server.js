e3fe949e8214add32051902ef62f668fe512fd2c
AinoMegumi <aimegu2014@gmail.com>
2022-09-02 00:22:30 +0900

    ファイル名を変更

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const multer = require('multer');
-const filectrl = require('./filecontrol.js');
+const sortFile = require('./sortfile.js');
 const upload = multer({ dest: 'files/' });
 
 const app = express();
 app.post('/api/upload', upload.array('files'), (req, res) => {
     let id = null;
     if (req.files == null || req.files.length === 0) return res.sendStatus(400);
-    req.files.forEach(f => (id = filectrl(f, id)));
+    req.files.forEach(f => (id = sortFile(f, id)));
     console.log(id);
     res.send(id);
 });
