bdc68b5493f6c4ac24d57f75745c5e06062135e7
AinoMegumi <aimegu2014@gmail.com>
2022-09-02 22:15:01 +0900

    ポートをJSONで指定できるように変更

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const multer = require('multer');
 const sortFile = require('./sortfile.js');
+const fs = require('fs');
+const config = JSON.parse(fs.readFileSync('./serverconfig.json'));
 const upload = multer({ dest: path.dirname(__dirname).replace(/\\/g, '/') + '/files/' });
-
 const app = express();
+
 app.post('/api/upload', upload.array('files'), (req, res) => {
     let id = null;
     if (req.files == null || req.files.length === 0) return res.sendStatus(400);
@@ -14,4 +16,4 @@ app.post('/api/upload', upload.array('files'), (req, res) => {
 });
 
 app.use(express.static('./wwwroot'));
-app.listen(process.env.HTTP_PLATFORM_PORT || 21000);
+app.listen(process.env.HTTP_PLATFORM_PORT || config.port);
