868bd666159b7f7848b64f13180a0dff4ace00d4
AinoMegumi <aimegu2014@gmail.com>
2022-09-02 22:36:37 +0900

    ファイルの全削除関数を追加

--- fileinspection.js
+++ fileinspection.js
@@ -1,8 +1,22 @@
+const fs = require('fs');
 const MimeTypeReg = RegExp('^audio/*[a-zA-Z0-9]{1,}$');
 
-/**
- * 投稿された全ファイルを検査する
- * @param {{ fieldname: string, originalname: string, encoding: string, mimetype: string, destination: string, filename: string, path: string. size: number }[]} FileInformations
- * @returns {boolean}
- */
-module.exports = (FileInformations) => FileInformations.some(f => !MimeTypeReg.test(f.mimetype));
+module.exports = {
+    /**
+     * 投稿された全ファイルを検査する
+     * @param {{ fieldname: string, originalname: string, encoding: string, mimetype: string, destination: string, filename: string, path: string. size: number }[]} FileInformations
+     */
+    deleteAll: FileInformations => {
+        FileInformations.forEach(f => {
+            fs.unlink(f.path, er => {
+                if (er) console.log(er);
+            });
+        });
+    },
+    /**
+     * 投稿された全ファイルを検査する
+     * @param {{ fieldname: string, originalname: string, encoding: string, mimetype: string, destination: string, filename: string, path: string. size: number }[]} FileInformations
+     * @returns {boolean}
+     */
+    inspectAll: FileInformations => FileInformations.some(f => !MimeTypeReg.test(f.mimetype)),
+};
