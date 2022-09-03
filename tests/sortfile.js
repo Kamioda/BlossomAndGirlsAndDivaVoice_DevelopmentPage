0753eac3105ad82838de7d8038aeddcd8f25c447
AinoMegumi <aimegu2014@gmail.com>
2022-09-03 14:29:56 +0900

    sortfile.jsのtest caseを修正

--- tests/sortfile.js
+++ tests/sortfile.js
@@ -3,6 +3,12 @@ const assert = require('assert');
 const fs = require('fs');
 
 describe('sortFile', () => {
+    before(() => {
+        if (!fs.existsSync('./files/')) fs.mkdirSync('./files/');
+        if (!fs.existsSync('./files/BA9189358FEE4EDA86457051E1EA455C/')) fs.mkdirSync('./files/BA9189358FEE4EDA86457051E1EA455C/');
+        if (!fs.existsSync('./files/testfile01')) fs.writeFileSync('./files/testfile01', 'HelloWorld');
+        if (!fs.existsSync('./files/testfile02')) fs.writeFileSync('./files/testfile02', 'HelloWorld');
+    });
     // 第１引数がnullの時の挙動テスト（第２引数の値は何だとしても関係ない）
     it('first arg is null', () => {
         assert.equal(sortFile(null, null), null);
@@ -29,7 +35,6 @@ describe('sortFile', () => {
 
     // 第１引数の値が正常（新規）
     it('file info is valid', () => {
-        if (!fs.existsSync('./files/testfile')) fs.writeFileSync('./files/testfile', 'HelloWorld');
         const result = sortFile(
             {
                 fieldname: '',
@@ -37,8 +42,8 @@ describe('sortFile', () => {
                 encoding: '',
                 mimetype: '',
                 destination: '',
-                filename: 'testfile',
-                path: './files/testfile',
+                filename: 'testfile01',
+                path: './files/testfile01',
                 size: 0,
             },
             null
@@ -50,8 +55,7 @@ describe('sortFile', () => {
     });
 
     // 第１引数の値が正常（追加）
-    it('file info is valid', () => {
-        if (!fs.existsSync('./files/testfile')) fs.writeFileSync('./files/testfile', 'HelloWorld');
+    it('file info is valid(append)', () => {
         const result = sortFile(
             {
                 fieldname: '',
@@ -59,13 +63,17 @@ describe('sortFile', () => {
                 encoding: '',
                 mimetype: '',
                 destination: '',
-                filename: 'testfile',
-                path: './files/testfile',
+                filename: 'testfile02',
+                path: './files/testfile02',
                 size: 0,
             },
             'BA9189358FEE4EDA86457051E1EA455C'
         );
         assert.equal(result, 'BA9189358FEE4EDA86457051E1EA455C');
         assert.equal(fs.existsSync('./files/BA9189358FEE4EDA86457051E1EA455C/helloworld.txt'), true);
+    });
+
+    after(() => {
+        if (fs.existsSync('./files/testfile')) fs.unlinkSync('./files/testfile', 'HelloWorld');
     });
 });
